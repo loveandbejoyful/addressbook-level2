73b33c862e498b474b551c14a49b7c8143e3f251
Yu <jeanette96@gmail.com>
2017-02-09 15:38:42 +0800

    Remove unnecessary methods from Printable interface

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,10 +1,5 @@
 package seedu.addressbook.data.person;
 
 public interface Printable {
-
-	String toString();
     String getPrintableString();
-    boolean equals(Object other);
-    int hashCode();
-	
 }
